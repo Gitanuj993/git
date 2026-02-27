1304393031c9136cce956cc4d288bb69d0de4f73
Gitanuj993 <anujtawar42@gmail.com>
2026-02-27 18:45:20 +0530

    grep file created and command using grep is included

---
+++ grep.cmd
@@ -0,0 +1,23 @@
+consider a file name ReadMe.md and contain a sentences ,
+a word 'repo' is mentioned in it .
+
+let us check this word using command 'grep'
+
+cmd goes like this - > grep 'repo' ReadMe.md
+
+above command will print all  the sentence  which includes word ' repo' 
+
+if the word doesn't present it doesn't print anything
+
+
+<h1> New Command </h1>
+<div align = " center " > 
+<p>
+<span>
+<h2> ls | grep '.md' </h2>
+<p> it prints all the files which has '.md' we can try another things as well to search in file names </p> 
+
+</span>
+
+</p>
+</div>
